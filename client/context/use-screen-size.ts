c7174efcc15ea9680036e897f7cddfce72bb43f3
Quintin Russell <quinn.j.russell@gmail.com>
2022-07-19 00:18:23 +0000

    figuring out tsc Provider

--- client/context/use-screen-size.ts
+++ client/context/use-screen-size.ts
@@ -3,16 +3,17 @@ import { createContext, useContext } from 'react';
 interface ScreenSizeContextType {
   width: number;
   height: number;
+  Provider: any;
 }
 
 const ScreenSizeContext = createContext<ScreenSizeContextType | undefined>(
-  undefined,
+  undefined
 );
 export default (): ScreenSizeContextType => {
   let screenSizeContext = useContext(ScreenSizeContext);
   if (screenSizeContext === undefined) {
     throw Error(
-      'ScreenSizeContext must be used inside of the App.tsx Component or it will not function properly.',
+      'ScreenSizeContext must be used inside of the App.tsx Component or it will not function properly.'
     );
   }
   return screenSizeContext;
--- client/index.tsx
+++ client/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 const container = document.querySelector('#root');
+console.log(container);
 const root = ReactDOM.createRoot(container);
 
 root.render(<App />);
